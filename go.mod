f475d8d2c056116475bd4517cc5a049b4a364911
Varun <varun.py10@gmail.com>
2023-01-19 18:50:00 +0530

    erp login in test

--- go.mod
+++ go.mod
@@ -8,20 +8,28 @@ require (
 	github.com/antchfx/htmlquery v1.2.5 // indirect
 	github.com/antchfx/xmlquery v1.3.13 // indirect
 	github.com/antchfx/xpath v1.2.1 // indirect
+	github.com/chromedp/cdproto v0.0.0-20230109101555-6b041c6303cc // indirect
+	github.com/chromedp/chromedp v0.8.7 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.2 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.1.0 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
--- main.go
+++ main.go
@@ -2,21 +2,38 @@ package main
 
 import (
 	//"encoding/json"
+
+	"context"
+	"crypto/sha1"
+	"crypto/sha256"
+	"log"
 	"net/http"
-
-	//"fmt"
 	"os"
-	//"unsafe"
+	"sort"
+	"strings"
+	"time"
 
+	"github.com/chromedp/chromedp"
 	"github.com/gin-gonic/gin"
 
 	"github.com/gocolly/colly"
-	//"github.com/google/go-cmp/cmp"
 )
 
 type Message struct {
-	TEXT_des string `json:"text_des"`
-	LINK     string `json:"link"`
+	TEXT_des string    `json:"text_des"`
+	LINK     []string  `json:"link"`
+	Date     time.Time `json:"date"`
+}
+type Profile struct {
+	ROLLNUMBER string `json:"id"`
+	NAME       string `json:"name"`
+	PROGRAM    string `json:"program"`
+	BRANCH     string `json:"branch"`
+	SEMESTER   string `json:"semester"`
+	EMAIL      string `json:"email"`
+	DOB        string `json:"dob"`
+	ADDRESS    string `json:"address"`
+	PHONE      string `json:"phonenumber"`
 }
 
 func delChar(s string) string {
@@ -27,54 +44,127 @@ func delChar(s string) string {
 	return ss
 }
 
+func set_time_date(date string) time.Time {
+	if date == "" {
+
+		t, _ := time.Parse("2006/01/02", "2000/01/01")
+		return t
+	}
+
+	temp := strings.Split(date, ".")
+	year := temp[2]
+	month := temp[1]
+	day := temp[0]
+	t, _ := time.Parse("2006/01/02", year+"/"+month+"/"+day)
+	return t
+
+}
+
 var prev_alltext = make([]Message, 0)
+var current_profile Profile
 
-func webscraping() []Message {
-	alltext := make([]Message, 0)
-	site_web_address := "http://www.dtu.ac.in/"
-	c := colly.NewCollector()
-	c.UserAgent = "Go program"
+func webscraping_latest_notices() {
+	for range time.Tick(time.Second * 19) {
 
-	c.OnHTML(".latest_tab li h6 a", func(e *colly.HTMLElement) {
+		alltext := make([]Message, 0)
+		site_web_address := "http://www.dtu.ac.in/"
+		c := colly.NewCollector()
+		c.UserAgent = "Go program"
 
-		temp := Message{
-			TEXT_des: e.DOM.First().Text(),
-			LINK:     site_web_address + delChar(e.DOM.First().AttrOr("href", "")),
-		}
-		//fmt.Println("-----------------------------")
-		alltext = append(alltext, temp)
-		//fmt.Printf("%T", e.DOM.First().Text())
-		//fmt.Println(e.DOM.First().Text())
-	})
+		c.OnHTML(".latest_tab li", func(e *colly.HTMLElement) {
+			links_array := e.ChildAttrs("a", "href")
 
-	//fmt.Print(alltext[1])
+			for i := 0; i < len(links_array); i++ {
+				links_array[i] = site_web_address + delChar(links_array[i])
+			}
+			notice_date := e.ChildText("small em i")
 
-	c.Visit("http://www.dtu.ac.in/")
-	//enc := json.NewEncoder(os.Stdout)
-	//enc.SetIndent("", " ")
-	//enc.Encode(alltext)
-	// if cmp.Equal(alltext, prev_alltext) {
-	// 	for i := 0; i < int(unsafe.Sizeof(prev_alltext)); i++ {
-	// 		if(alltext[i]!=prev_alltext[i])
-	// 	}
+			notice_date_time := set_time_date(notice_date)
 
-	// }
-	prev_alltext = alltext
-	return prev_alltext
+			temp := Message{
+				TEXT_des: e.ChildText("h6 a"),
+				LINK:     links_array,
+				Date:     notice_date_time,
+			}
+
+			alltext = append(alltext, temp)
+
+			// fmt.Printf("%T", e.DOM.First().Text())
+			// fmt.Println(e.DOM.First().Text())
+		})
+
+		c.Visit("http://www.dtu.ac.in/")
+		// enc := json.NewEncoder(os.Stdout)
+		// enc.SetIndent("", " ")
+		// enc.Encode(alltext)
+		sort.Slice(alltext, func(i, j int) bool { return alltext[i].Date.After(alltext[j].Date) })
+		prev_alltext = alltext
+
+	}
 }
+
 func getNotice(c *gin.Context) {
-	x := webscraping()
-	c.IndentedJSON(http.StatusOK, x)
+	c.IndentedJSON(http.StatusOK, prev_alltext)
 }
+
+func getuserinfo(c *gin.Context) {
+	erp_login_page(c.Query("id"), c.Query("password"))
+	c.IndentedJSON(http.StatusOK, current_profile)
+}
+
+func erp_login_page(s1, s2 string) {
+
+	current_profile.ROLLNUMBER = s1
+	ctx, cancel := chromedp.NewContext(context.Background(), chromedp.WithLogf(log.Printf))
+	defer cancel()
+	chromedp.Run(ctx, chromedp.Navigate("https://cumsdtu.in/student_dtu/login/login.jsp"),
+
+		chromedp.SetValue("usernameId", s1),
+		chromedp.SetValue("passwordId", s2),
+		chromedp.Click("submitButton"),
+		chromedp.Sleep(time.Second*3),
+		chromedp.Click("Link145"),
+		chromedp.Text(`ListItem248`, &current_profile.ADDRESS),
+		chromedp.Text(`ListItem248`, &current_profile.NAME),
+		chromedp.Text(`ListItem248`, &current_profile.PROGRAM),
+		chromedp.Text(`ListItem248`, &current_profile.BRANCH),
+		chromedp.Text(`Label621`, &current_profile.SEMESTER),
+		chromedp.Text(`ListItem193`, &current_profile.EMAIL),
+		chromedp.Click("Layer_1-2"),
+	)
+
+}
+
 func main() {
+
+	go webscraping_latest_notices()
+
 	router := gin.Default()
 	router.GET("/notices", getNotice)
+	router.GET("/test", getuserinfo)
+
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "localhost:8080"
+		port = "localhost:8081"
 	} else {
 		port = ":" + port
 	}
 	router.Run(port)
 
+}
+func calsha1(str string) string {
+
+	sha := sha1.New()
+	sha.Write([]byte(str))
+	digest := sha.Sum(nil)
+	return string(digest)
+
+}
+
+func calsha256(str string) string {
+
+	sha := sha256.New()
+	sha.Write([]byte(str))
+	digest := sha.Sum(nil)
+	return string(digest)
 }
